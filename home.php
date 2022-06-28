57566b09d3918401b072e0316d326cf3100e5267
itcatmeow <103352895+itcatmeow@users.noreply.github.com>
2022-06-28 20:38:09 +0800

    homepage

---
+++ home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home</title>
+    </head>
+    <style>
+    </style>
+    <body>
+        <H1>Student Management System</H1>
+    </body>
+</html>
