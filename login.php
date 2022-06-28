b405796d04a9c2ca5ac9da8ea2101c12024df7f9
itcatmeow <103352895+itcatmeow@users.noreply.github.com>
2022-06-28 21:16:36 +0800

    login student php

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+    $con = mysqli_connect("localhost", "root", "") or die ("Unable to connect");
+    mysqli_select_db($con, "studentmanagementsystem");
+?>
+<?php
+if(isset($_POST['login']))
+{
+    $id = $_POST['id'];
+    $password = $_POST['password'];
+    $sql = "SELECT * FROM student WHERE student_id='$_POST[id]' AND student_password='$_POST[password]'";
+
+    $result = mysqli_query($con, $sql);
+
+    if(mysqli_num_rows($result)>0)
+    {
+        $_SESSION['student_id']=$_POST['id'];
+        header('location:home.php');
+    }
+    else
+    {
+        echo "<script type'text/javascript'>alert('Invalid Student ID or password!')</script>";  
+    }
+}
+?>
