5d515a64a6a5c913d0950196e91aecf8d9201929
lmanoob <103398600+lmanoob@users.noreply.github.com>
2022-06-30 20:55:22 +0800

    Update deleteclass.php

--- deleteclass.php
+++ deleteclass.php
@@ -1,7 +1,7 @@
 <?php
 include 'conn.php';
 $class_code = $_GET['class_code'];
-$delete =  "delete from user where class_code=$class_code";
+$delete =  "delete from class where class_code=$class_code";
 if (!mysqli_query($conn, $delete)){
     die ( 'Error: '. mysqli_error($conn));
 }
