cd692b0e500d9d0d8f8f354a5d7427e26264c268
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-06-30 21:50:40 +0800

    Create home_student.php

---
+++ home_student.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home</title>
+    <?php
+	include 'conn.php';
+	session_start();
+	?>
+    </head>
+    <style>
+    </style>
+    <body>
+        <H1>Student Management System</H1>
+		<a href=studentlist.php>Student list</a>
+        <a href=register_class.php?>Register a class</a>
+    </body>
+</html>
