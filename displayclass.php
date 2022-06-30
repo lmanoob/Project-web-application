06731631d5e2588f2bada6370f79c0342d2973ad
itcatmeow <103352895+itcatmeow@users.noreply.github.com>
2022-06-30 19:59:38 +0800

    Update displayclass.php

--- displayclass.php
+++ displayclass.php
@@ -1,7 +1,7 @@
 <?php
 include 'conn.php';
 echo "<table border ='1'><tr><th>Class Code</th><th>Class Name</th>";
-echo "<th>Number of Student</th><th>Subject Code</th><th>Teacher ID</th><th>Action</th></tr>";
+echo "<th>Number of Student</th><th>Subject Code</th><th>Teacher ID</th></tr>";
 $result = mysqli_query($conn, "select * from class");
 while($row = mysqli_fetch_row($result)){
 	echo"<tr><td><a href=classdetails.php?class_code=$row[0]>$row[0]</a></td>";
@@ -9,9 +9,6 @@ while($row = mysqli_fetch_row($result)){
 	echo"<td>$row[2]</td>";
 	echo"<td>$row[3]</td>";
 	echo"<td>$row[4]</td>";
-	echo"<td><a href=update.php?userid=$row[0]>Update</a></td>";
-	echo"&nbsp;&nbsp;&nbsp;";
-	echo"<td><a href=delete.php?userid=$row[0]>Delete</a></td>";
 	echo"</tr>";
 }echo"</table>";
 mysqli_free_result($result);
