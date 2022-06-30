bba33005d8c1e57cc472de235840eeb34bf75b6b
lmanoob <103398600+lmanoob@users.noreply.github.com>
2022-06-30 20:39:24 +0800

    Create deleteclass.php

---
+++ deleteclass.php
@@ -0,0 +1,11 @@
+<?php
+include 'conn.php';
+$userid = $_GET['class_code'];
+$delete =  "delete from user where class_code=$class_code";
+if (!mysqli_query($conn, $delete)){
+    die ( 'Error: '. mysqli_error($conn));
+}
+    echo "Sucessfully deleted<br>";
+    echo "<a href=displaysclass.php>Back to table</a>";
+mysqli_close($conn);
+?>
