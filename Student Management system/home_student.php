b60d81bb6a4b8e1103ccda09d1f7d8bc5886c2e9
itcatmeow <103352895+itcatmeow@users.noreply.github.com>
2022-07-05 18:16:40 +0800

    Changing the css

--- Student Management system/home_student.php
+++ Student Management system/home_student.php
@@ -3,39 +3,32 @@
 	session_start();
     $id = $_SESSION['student_id'];
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Home</title>
-    </head>
-    <style>
-        body{
-            text-align:center;
-            background: rgb(2,0,36);
-            background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(95,9,121,1) 100%, rgba(0,212,255,1) 100%);
-        }
-        h1{
-            color:white;
-            font-family: sans-serif;
-        }
-        a:link, a:visited {
-        background-color: #f44336;
-        color: white;
-        padding: 14px 25px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        }
-
-        a:hover, a:active {
-        background-color: red;
-        }
-    </style>
-    <body>
-        <H1>Student Management System</H1>
-		<a href=studentprofile.php>View Profile</a>
-        <a href=register_class.php>Register a class</a>
-        <a href=viewownclass.php>View registered class</a>
-        <a href=viewownsubject.php>View registered subject</a>
-    </body>
+<!Doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Home</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">Student Management System</span>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link active" aria-current="page" href="home_student.php">Home</a>
+              <a class="nav-link" href="studentprofile.php">Profile</a>
+              <a class="nav-link" href="register_class.php">Class Registration</a>
+              <a class="nav-link" href="viewownclass.php">Class</a>
+              <a class="nav-link" href="viewownsubject.php">Subject</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
 </html>
