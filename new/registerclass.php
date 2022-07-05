af3d0351addd2a7d1bccde6ab6cf87b7d7e73f0b
itcatmeow <103352895+itcatmeow@users.noreply.github.com>
2022-07-05 10:39:25 +0800

    Add files via upload

---
+++ new/registerclass.php
@@ -0,0 +1,11 @@
+<?php
+include 'conn.php';
+
+$sql="insert into register (class_code, student_id)
+values ('$_POST[id]','$_POST[class_code]')";
+if(!mysqli_query($conn, $sql))
+{
+    die('Error: '.mysqli_error($conn));
+}
+echo "Successfully registered <br>";
+?>
