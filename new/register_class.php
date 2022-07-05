987266e16f30b6cc25c538308322f1eb96e1106e
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-05 12:25:56 +0800

    Update register_class.php

--- new/register_class.php
+++ new/register_class.php
@@ -7,13 +7,14 @@
 	$result2 = mysqli_query($conn, "SELECT student_id FROM student WHERE student_id='$id'");
 	$row = mysqli_fetch_row($result2);
 	$row2 = mysqli_fetch_row($result);
-	echo "<form action=registerclass.php method = post>";
+	echo "<form action=registerclass.php method =post>";
 	echo "Student ID: <input type=text name=id value=$row[0] readonly><br>";
 	echo "<label for=cars>Pick A Class:</label><br>";
-	echo "<select>";
+	echo "<select name='class_code'>";
 	foreach( $result as $row2 ){
-	echo "<option value='class_code'>" . $row2['class_code'] . "</option>";}
+	$class_code=$row2['class_code'];
+	echo "<option value=".$class_code.">" . $row2['class_code'] . "</option>";}
 
 	echo "</select><br>";
-	echo "<input type=submit value=Register>";
+	echo "<input type=submit value=Register></form>";
 ?>
