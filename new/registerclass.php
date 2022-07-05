4055154503fa07870a3afbc3ea8ee0cbdc89e772
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-05 12:25:25 +0800

    Update registerclass.php

--- new/registerclass.php
+++ new/registerclass.php
@@ -1,8 +1,7 @@
 <?php
 include 'conn.php';
-
 $sql="insert into register (class_code, student_id)
-values ('$_POST[id]','$_POST[class_code]')";
+values ('$_POST[id]','$_POST[class_code])";
 if(!mysqli_query($conn, $sql))
 {
     die('Error: '.mysqli_error($conn));
