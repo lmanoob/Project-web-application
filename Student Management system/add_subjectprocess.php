9eee579c1c6700f4836cb3f6737c290c263def24
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-06 20:45:16 +0800

    Create add_subjectprocess.php

---
+++ Student Management system/add_subjectprocess.php
@@ -0,0 +1,11 @@
+<?php
+include 'conn.php';
+
+$sql="insert into subject (subject_code, subject_name, subject_desc, subject_credit)
+values ('$_POST[sub_code]','$_POST[sub_name]','$_POST[sub_desc]','$_POST[sub_cred]')";
+if(!mysqli_query($conn, $sql))
+{
+    die('Error: '.mysqli_error($conn));
+}
+echo "Successfully registered <br>";
+?>
