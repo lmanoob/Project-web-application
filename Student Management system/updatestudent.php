eda2607e09c01bd224e17e83f9f6d185675047ce
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-06 22:09:15 +0800

    Create updatestudent.php

---
+++ Student Management system/updatestudent.php
@@ -0,0 +1,17 @@
+<?php
+include 'conn.php';
+$id = $_GET['id'];
+$result = mysqli_query($conn, "select * from student
+where student_id='$id'");
+$row = mysqli_fetch_row($result);
+    echo "<form action=update_processstudent.php?id=$row[0] method=post>";
+    echo "Student ID: <input type='text' name='id' value='$row[0]' readonly><br>";
+    echo  "Firstname: <input type='text' name='firstname' value='$row[1]'><br>";
+    echo  "Lastname: <input type='text' name='lastname'  value='$row[2]'><br>";
+    echo  "Email: <input type='text' name='email' value='$row[3]'><br>";
+    echo  "Password: <input type='text' name='password' value='$row[4]'><br>";
+    echo  "Phone Number: <input type='text' name='phone' value='$row[5]'><br>";
+echo "<input type=submit value=Update>";
+mysqli_free_result($result);
+mysqli_close($conn);
+?>
