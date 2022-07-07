d8772d987157d1e613b27eb998a70d921d1c06bd
itcatmeow <103352895+itcatmeow@users.noreply.github.com>
2022-07-07 16:12:48 +0800

    Update subjectlist.php

--- Student Management system/subjectlist.php
+++ Student Management system/subjectlist.php
@@ -1,20 +1,47 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Home</title>
+    <style>
+      body{
+        background-image: url("Image/background.jpg");
+        background-color: aquamarine;
+        height:400px;
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+      }
+    </style>
+  </head>
+  <body>
 <?php
 include 'conn.php';
-echo "<table border ='1'><tr><th>Subject Code</th><th>Subject Name</th>";
-echo "<th>Subject Description</th><th>Subject Credit</th><th>Action</th></tr>";
+include "./header_teach.html";
+echo "<br>";
 $result = mysqli_query($conn, "select * from subject");
+echo "<div class=container>";
+echo "<div class=row>";
+echo "<table class=table>";
 while($row = mysqli_fetch_row($result)){
-	echo"<tr><td>$row[0]</td>";
-	echo"<td>$row[1]</a></td>";
-	echo"<td>$row[2]</td>";
-	echo"<td>$row[3]</td>";
-	echo"<td><a href=updatesubject.php?subject_code=$row[0]>Update</a></td>";
-	echo"&nbsp;&nbsp;&nbsp;";
-	echo"<td><a href=deletesubject.php?subject_code=$row[0]>Delete</a></td>";
-	echo"</tr>";
-}echo"</table>";
-echo "<a href=home_teacher.php>Home</a>";
-echo "<button type=button><a href=add_subject.php>New Subject</a></button>";
+	echo "<tr class=bg-primary><th>Subject Code</th><th scope=col>$row[0]</th></tr>";
+    echo "<tr class=bg-info><th>Subject Name</th><th scope=col>$row[1]</th></tr>";
+    echo "<tr class=bg-info><th>Subject Description</th><th scope=col>$row[2]</th></tr>";
+    echo "<tr class=bg-info><th>Subject Credit</th><th scope=col>$row[3]</th></tr>";
+    echo "<tr class=bg-secondary><th>Action </th><th scope=col><a class=link-info href=updatesubject.php?subject_code=$row[0]> Update </a><a class=link-info href=deletesubject.php?subject_code=$row[0]> Delete </a></th></tr>";
+
+}
+echo "</table>";
+echo "</div>";
+echo "</div>";
+echo "<div class='col-md-12 text-center'>";
+echo "<button type=button class='btn btn-dark'><a class=link-light href=add_subject.php>Add a subject</a></button>";
+echo "</div>";
 mysqli_free_result($result);
 mysqli_close($conn);
 ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+    </html>
