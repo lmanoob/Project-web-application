61f760dd8c658aab684b94acce60485bd035e5c8
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-07 11:36:11 +0800

    tambah css bootstrap

--- Student Management system/registerclass.php
+++ Student Management system/registerclass.php
@@ -1,10 +1,62 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Home</title>
+    <style>
+      body{
+        background-image: url("Image/background.jpg");
+        background-color: aquamarine;
+        height:100%;
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+      }
+       #kotak-tengah{
+         border-radius: 50px;
+         background-color: white;
+         width: 30%;
+         margin:auto; /* Center the div */
+         position:relative;
+         padding: 50px;
+		 top: 50%;
+        }
+    </style>
+  </head>
+<body>
 <?php
-include 'conn.php';
-$sql="insert into register (class_code, student_id)
-values ('$_POST[class_code]','$_POST[id]')";
-if(!mysqli_query($conn, $sql))
-{
-    die('Error: '.mysqli_error($conn));
-}
-echo "Successfully registered <br>";
+	session_start();
+	include 'conn.php';
+	include './header.html';
+	
+	$id= $_SESSION['student_id'];
+	$result = mysqli_query($conn, "SELECT class_code FROM class");
+	$result2 = mysqli_query($conn, "SELECT student_id FROM student WHERE student_id='$id'");
+	$row = mysqli_fetch_row($result2);
+	$row2 = mysqli_fetch_row($result);
+	
+	echo "<div id='kotak-tengah'>";
+    echo "<form action=registerclass.php?method=post>";
+    
+    echo "<div class=mb-3>";
+    echo "<label for='id' class=form-label>Student ID</label>"; 
+    echo "<input type='text' class=form-control name='id' value='$row[0]' disabled>";
+    echo "</div>";
+	
+	echo "<div class=mb-3>";
+	echo "<label for='class_code'>Pick A Class:</label><br>";
+	echo "<select class='form-select aria-label='Default select example' 'name='class_code'>";
+	echo "<option selected>Class</option>";
+	foreach( $result as $row2 ){
+	$class_code=$row2['class_code'];
+	echo "<option value=".$class_code.">" . $row2['class_code'] . "</option>";}
+	echo "</select><br>";
+	echo "</div>";
+	echo "<input type=submit value=Register></form>";
+	echo "</div>";
+	echo "<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js'></script>";
+    echo "</body>";
+    echo "</html>";
 ?>
