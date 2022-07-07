5193afdd48241077ee036e594d611eb6124a5e38
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-07 12:47:17 +0800

    Update update_processstudent.php

--- Student Management system/update_processstudent.php
+++ Student Management system/update_processstudent.php
@@ -1,17 +1,18 @@
 <?php
 include 'conn.php';
 $id = $_GET['id'];
-$update = "UPDATE student SET student_firstname='$_POST[firstname]',
-student_lastname='$_POST[lastname]',student_email='$POST[email]',
-student_password='$_POST[password]',student_phoneNum='$_POST[phone]')
+$newid = $_POST['newid'];
+$update = "UPDATE student SET student_id='$_POST[newid]', student_firstname='$_POST[firstname]',
+student_lastname='$_POST[lastname]',student_email='$_POST[email]',
+student_password='$_POST[password]',student_phoneNum='$_POST[phone]'
 where student_id='$id'";
 if(!mysqli_query($conn, $update)){ die('Error: '.mysqli_error($conn)); }
-$result = mysqli_query($conn, "select * from student where student_id='$id'");
+$result = mysqli_query($conn, "select * from student where student_id='$newid'");
 $row = mysqli_fetch_row($result);
-    echo "User ID : $row[0]<br>First name : $row[1]><br>";
-    echo "Last name : $row[2]><br>Email : $row[3]";
-    echo "Password : $row[4]><br>Student phone number : $row[5]<br>";
-    echo "<ahref=studentlist.php>Back to table</a>";
+    echo "Student ID : $row[0]<br>First name : $row[1]<br>";
+    echo "Last name : $row[2]<br>Email : $row[3]";
+    echo "Password : $row[4]<br>Student phone number : $row[5]<br>";
+    echo "<a href=studentlist.php>Back to table</a>";
 mysqli_free_result($result);
 mysqli_close($conn);
 ?>
