6ff3f345d3f39f2197567607ebb8d6a8b4937d95
itcatmeow <103352895+itcatmeow@users.noreply.github.com>
2022-07-07 13:30:23 +0800

    Update viewownsubject.php

--- Student Management system/viewownsubject.php
+++ Student Management system/viewownsubject.php
@@ -1,15 +1,45 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Home</title>
+    <style>
+      body{
+        background-image: url("Image/background.jpg");
+        background-color: aquamarine;
+        height:400px;
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+      }
+    </style>
+  </head>
+  <body>
 <?php
 	session_start();
 	include 'conn.php';
-
+    include "./header.html";
+    echo "<br>";
 	$id= $_SESSION['student_id'];
-    $result = mysqli_query($conn, "SELECT * from subject where subject_code=(select subject_code from class where class_code=(select class_code from register where student_id='$id'))");
-    $row = mysqli_fetch_row($result);
-    echo "Subject Code: $row[0]<br>";
-    echo "Subject Name: $row[1]<br>";
-    echo "Subject Description: $row[2]<br>";
-    echo "Subject Credit: $row[3]<br>";
+    $result = mysqli_query($conn, "SELECT * from subject where subject_code IN (select subject_code from class where class_code IN (select class_code from register where student_id='$id'))");
+    echo "<div class=container>";
+    echo "<div class=row>";
+    echo "<table class=table>";
+    while ($row = mysqli_fetch_row($result)){
+        echo "<tr class=bg-primary><th>Subject Code</th><th scope=col>$row[0]</th></tr>";
+        echo "<tr class=bg-info><th>Subject Name</th><th scope=col>$row[1]</th></tr>";
+        echo "<tr class=bg-info><th>Subject Description</th><th scope=col>$row[2]</th></tr>";
+        echo "<tr class=bg-info><th>Subject Credit</th><th scope=col>$row[3]</th></tr>";
+    }
+    echo "</table>";
+    echo "</div>";
+    echo "</div>";
     mysqli_free_result($result);
     mysqli_close($conn);
 
 ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
