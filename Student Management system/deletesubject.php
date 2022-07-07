ecdd61f41c8c32646ad55cbd0f18092682376662
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-07 20:43:14 +0800

    Update deletesubject.php

--- Student Management system/deletesubject.php
+++ Student Management system/deletesubject.php
@@ -5,7 +5,10 @@ $delete =  "delete from subject where subject_code='$subject_code'";
 if (!mysqli_query($conn, $delete)){
     die ( 'Error: '. mysqli_error($conn));
 }
+else{
     echo "Sucessfully deleted<br>";
+    header('location:subjectlist.php');
     echo "<a href=subjectlist.php>Back to table</a>";
 mysqli_close($conn);
+}
 ?>
