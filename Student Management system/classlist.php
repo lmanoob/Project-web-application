81c70709a51ff53fadc098fec945319d8a0d63c9
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-12 10:11:20 +0800

    Update classlist.php

--- Student Management system/classlist.php
+++ Student Management system/classlist.php
@@ -9,7 +9,7 @@
       body{
         background-image: url("Image/background.jpg");
         background-color: aquamarine;
-        height:400px;
+        height:100%;
         background-position: center;
         background-size: cover;
         background-repeat: no-repeat;
@@ -25,23 +25,20 @@ $result = mysqli_query($conn, "select * from class");
 echo "<div class=container>";
 echo "<div class=row>";
 echo "<table class=table>";
+    echo "<tr class=bg-info><th>Class Code</th><th>Class Name</th><th>Number of Student</th><th>Subject Code</th><th>Teacher ID</th><th>Action </th></tr>";
 while($row = mysqli_fetch_row($result)){
-    echo "<tr class=bg-primary><th>Class Code</th><th scope=col>$row[0]</th></tr>";
-    echo "<tr class=bg-info><th>Class Name</th><th scope=col>$row[1]</th></tr>";
-    echo "<tr class=bg-info><th>Number of student</th><th scope=col>$row[2]</th></tr>";
-    echo "<tr class=bg-info><th>Subject code</th><th scope=col ><a href=subjectdetails.php?subject_code=$row[3]>$row[3]</a></th></tr>";
-    echo "<tr class=bg-info><th>Teacher ID</th><th scope=col>$row[4]</th></tr>";
-    echo "<tr class=bg-secondary><th>Action </th><th scope=col><a class=link-info href=updateclass.php?class_code=$row[0]> Update </a><a class=link-info href=deleteclass.php?class_code=$row[0]> Delete </a></th></tr>";
+    echo "<tr class=bg-info><th scope=col>$row[0]</th><th scope=col>$row[1]</th><th scope=col>$row[2]</th>><th scope=col >$row[3]</th><th scope=col>$row[4]</th>";
+	echo "<th class=bg-secondary scope=col><a class=link-info href=updateclass.php?class_code=$row[0]> Update </a><a class=link-info href=deleteclass.php?class_code=$row[0]> Delete </a></th></tr>";
 }
 echo "</table>";
 echo "</div>";
 echo "</div>";
 echo "<div class='col-md-12 text-center'>";
 echo "<button type=button class='btn btn-dark'><a class=link-light href=addclass.php>Add a class</a></button>";
+echo "<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js'></script>";
 echo "</div>";
 mysqli_free_result($result);
 mysqli_close($conn);
 ?>
-     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
