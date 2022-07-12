210ea087beea945290e7e5a981ed4d36d9ac8893
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-12 14:58:18 +0800

    Update registerclass.php

--- Student Management system/registerclass.php
+++ Student Management system/registerclass.php
@@ -1,10 +1,12 @@
 <?php
 include 'conn.php';
-$sql="insert into register (class_code, student_id)
+$sql="insert into register (class_code, student_id) 
 values ('$_POST[class_code]','$_POST[id]')";
 if(!mysqli_query($conn, $sql))
 {
     die('Error: '.mysqli_error($conn));
+}else{
+    header('location:viewownclass.php');
+	mysqli_close($conn);
 }
-echo "Successfully registered <br>";
 ?>
