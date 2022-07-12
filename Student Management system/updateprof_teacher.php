14d72aa362adb01c2cd43f7ae7419aab48ca93ba
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-12 14:33:09 +0800

    Update updateprof_teacher.php

--- Student Management system/updateprof_teacher.php
+++ Student Management system/updateprof_teacher.php
@@ -1,53 +1,18 @@
-<!Doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Home</title>
-    <style>
-      body{
-        background-image: url("Image/background.jpg");
-        background-color: aquamarine;
-        height:400px;
-        background-position: center;
-        background-size: cover;
-        background-repeat: no-repeat;
-      }
-    </style>
-  </head>
-  <body>
-	<?php
+<?php
 	session_start();
 	include 'conn.php';
-	include './header_teach.html';
-	echo "<br>";
+	$teacher_id= $_SESSION['teacher_id'];
 	$id=$_GET['id'];
 	$update = "UPDATE teacher SET teacher_firstName='$_POST[firstname]',
 	teacher_lastName='$_POST[lastname]',teacher_email='$_POST[email]',
 	teacher_password='$_POST[password]',teacher_phoneNum='$_POST[phone]'
-	where teacher_id='$id'";
+	where teacher_id='$teacher_id'";
 	if (!mysqli_query($conn, $update))
 	{ 
 		die ('Error: '.mysqli_error($conn));
-	}
-	$result = mysqli_query($conn, "select * from teacher where teacher_id='$id'");
-	$row = mysqli_fetch_row($result);
-	echo "<div class=container>";
-	echo "<div class=row>";
-	echo "<table class=table>";
-	echo "<tr class=bg-info><th>Teacher ID</th><th scope=col>$row[0]</th></tr>";
-	echo "<tr class=bg-info><th>First Name</th><th scope=col>$row[1]</th></tr>";
-	echo "<tr class=bg-info><th>Last Name</th><th scope=col>$row[2]</th></tr>";
-	echo "<tr class=bg-info><th>Email</th><th scope=col>$row[3]</th></tr>";
-	echo "<tr class=bg-info><th>Password</th><th scope=col>$row[4]</th></tr>";
-	echo "<tr class=bg-info><th>Phone Number</th><th scope=col>$row[5]</th></tr>";
-	echo "</table>";
-	echo "</div>";
-	echo "</div>";
-	mysqli_free_result($result);
+	}else{
+    echo "Sucessfully deleted<br>";
+    header('location:teacherprofile.php');
 	mysqli_close($conn);
-	?>
-	 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
-  </body>
-</html>
+}
+?>
