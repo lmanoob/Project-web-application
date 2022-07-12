6636690b42a3047eb29fb2249a2708967c643a07
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-12 14:23:17 +0800

    Update teach_class.php

--- Student Management system/teach_class.php
+++ Student Management system/teach_class.php
@@ -20,25 +20,22 @@
 <?php
 	session_start();
 	include 'conn.php';
-	include "./header_teach.html";
-	echo "<br>";
+	include './header_teach.html';
 	$id= $_SESSION['teacher_id'];
     $result = mysqli_query($conn, "SELECT * from class where teacher_id='$id'");
     echo "<div class=container>";
     echo "<div class=row>";
     echo "<table class=table>";
-    while($row = mysqli_fetch_row($result)){
-        echo "<tr class=bg-primary><th>Class Code</th><th scope=col>$row[0]</th></tr>";
-    	echo "<tr class=bg-info><th>Class Name</th><th scope=col>$row[1]</th></tr>";
-   		echo "<tr class=bg-info><th>Number of student</th><th scope=col>$row[2]</th></tr>";
-    	echo "<tr class=bg-info><th>Subject code</th><th scope=col>$row[3]</th></tr>";
-    	echo "<tr class=bg-info><th>Teacher ID</th><th scope=col>$row[4]</th></tr>";
-    }
+    echo "<tr class=bg-secondary><th>Class Code</th><th>Class Name</th><th>Number of students</th><th>Subject Code</th><th>Teacher ID </th></tr>";
+while($row = mysqli_fetch_row($result)){
+    echo "<tr class=bg-info><th scope=col>$row[0]</th><th scope=col>$row[1]</th><th scope=col>$row[2]</th><th scope=col>$row[3]</th><th scope=col>$row[4]</th></tr>";
+
+}
     echo "</table>";
     echo "</div>";
     echo "</div>";
-    mysqli_free_result($result);
-    mysqli_close($conn);
+	mysqli_free_result($result);
+	mysqli_close($conn);
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
