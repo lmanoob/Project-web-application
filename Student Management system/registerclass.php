e5bccffa5fde91698e951f3525d6b0750c9d3e17
lmanoob <103398600+lmanoob@users.noreply.github.com>
2022-07-13 10:20:53 +0800

    add comment

--- Student Management system/registerclass.php
+++ Student Management system/registerclass.php
@@ -1,12 +1,12 @@
 <?php
-include 'conn.php';
+include 'conn.php'; //call conn
 $sql="insert into register (class_code, student_id) 
-values ('$_POST[class_code]','$_POST[id]')";
+values ('$_POST[class_code]','$_POST[id]')"; //function for sql query
 if(!mysqli_query($conn, $sql))
 {
     die('Error: '.mysqli_error($conn));
 }else{
     header('location:viewownclass.php');
-	mysqli_close($conn);
+	mysqli_close($conn); // close the sql section
 }
 ?>
