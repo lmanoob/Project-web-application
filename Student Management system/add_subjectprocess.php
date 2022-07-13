3792ee9d15569641a0680f718c739f265a4a08c8
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-13 10:38:30 +0800

    Update add_subjectprocess.php

--- Student Management system/add_subjectprocess.php
+++ Student Management system/add_subjectprocess.php
@@ -1,13 +1,16 @@
 <?php
 include 'conn.php';
 
+//Send sql statement to database
 $sql="insert into subject (subject_code, subject_name, subject_desc, subject_credit)
 values ('$_POST[sub_code]','$_POST[sub_name]','$_POST[sub_desc]','$_POST[sub_cred]')";
 if(!mysqli_query($conn, $sql))
 {
+    //Output the type of error if fail
     die('Error: '.mysqli_error($conn));
 }
 else{
+    //Send user to the next page
      header('location:subjectlist.php');
 }
 ?>
