503025146d2044b2cea35f807559b6f86c4f8ab4
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-13 10:17:06 +0800

    Update add_studentprocess.php

--- Student Management system/add_studentprocess.php
+++ Student Management system/add_studentprocess.php
@@ -1,13 +1,16 @@
 <?php
 include 'conn.php';
 
+//send sql statement to the database
 $sql="insert into student (student_id, student_firstName, student_lastName, student_email, student_password, student_phoneNum)
 values ('$_POST[id]','$_POST[firstname]','$_POST[lastname]','$_POST[email]','$_POST[password]','$_POST[phone]')";
 if(!mysqli_query($conn, $sql))
 {
+    //output the type of error if fail 
     die('Error: '.mysqli_error($conn));
 }
 else{
+    // bring to next location if sucessfull
      header('location:studentlist.php');
 }
 ?>
