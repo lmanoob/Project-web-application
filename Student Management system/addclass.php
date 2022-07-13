beec8360ad6a0982a3ca6adabf5833d3d415ff04
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-13 11:12:01 +0800

    Update addclass.php

--- Student Management system/addclass.php
+++ Student Management system/addclass.php
@@ -77,6 +77,7 @@
 	echo "</select></div><br>";
 	echo "<input type='submit' class='btn btn-primary' value=Register></form>";
 	echo "</div>";
+	//link to implement javascript bootsrtap
 	echo "<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js'></script>";
     echo "</body>";
     echo "</html>";
