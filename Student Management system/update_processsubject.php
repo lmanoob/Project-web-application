70375d84fe3daa18d103c2a3388a16375dbb1111
itcatmeow <103352895+itcatmeow@users.noreply.github.com>
2022-07-13 12:03:17 +0800

    Update update_processsubject.php

--- Student Management system/update_processsubject.php
+++ Student Management system/update_processsubject.php
@@ -4,13 +4,14 @@ include './header_teach.html';
 echo "<br>";
 $subject_code = $_GET['subject_id'];
 $new_code=$_POST['SuCode'];
+//send sql statement to the database
 $update = "UPDATE subject SET subject_code='$_POST[SuCode]',subject_name='$_POST[SuName]',
 subject_desc='$_POST[SuDescript]',subject_credit='$_POST[credit]'
 where subject_code='$subject_code'";
 if(!mysqli_query($conn, $update)){ die('Error: '.mysqli_error($conn)); 
 }
 else{
-    header('location:subjectlist.php');
-	mysqli_close($conn);
+header('location:subjectlist.php');
+mysqli_close($conn);
 }
 ?>
