376f994810666a1f5c8f68218eb5488ab7d6ce6e
itcatmeow <103352895+itcatmeow@users.noreply.github.com>
2022-07-13 11:57:49 +0800

    Update updateprof.php

--- Student Management system/updateprof.php
+++ Student Management system/updateprof.php
@@ -2,6 +2,7 @@
 	session_start();
 	include 'conn.php';
 	$id=$_GET['id'];
+	//send sql statement to the database
 	$update = "UPDATE student SET student_firstName='$_POST[firstname]',
 	student_lastName='$_POST[lastname]',student_email='$_POST[email]',
 	student_password='$_POST[password]',student_phoneNum='$_POST[phone]'
@@ -10,8 +11,8 @@
 	{ 
 		die ('Error: '.mysqli_error($conn));
 	}else{
-    echo "Sucessfully deleted<br>";
-    header('location:studentprofile.php');
+    	echo "Sucessfully deleted<br>";
+    	header('location:studentprofile.php');
 	mysqli_close($conn);
 }
 ?>
