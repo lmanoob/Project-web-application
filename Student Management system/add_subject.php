dd373539bc789b436f1745adc92e4dbce03ccf37
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-13 10:35:41 +0800

    Update add_subject.php

--- Student Management system/add_subject.php
+++ Student Management system/add_subject.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+	//link to bootsrap to use the css
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Home</title>
     <style>
@@ -11,9 +12,10 @@
         background-color: aquamarine;
         height:100%;
         background-position: center;
-        background-size: cover;
+        background-size: cover; /*Background image will cover the whole page*/
         background-repeat: no-repeat;
       }
+	/* Css to put the content into the middle */
        #kotak-tengah{
          border-radius: 50px;
          background-color: white;
@@ -27,9 +29,9 @@
 <body>
 <?php
 include 'conn.php';
-include "./header_teach.html";
+include "./header_teach.html"; //header for navbar
 ?>
-		<div id='kotak-tengah'>
+	<div id='kotak-tengah'>
         <table>
         <form action="add_subjectprocess.php" method = "post">
 			<div class='mb-3'>
