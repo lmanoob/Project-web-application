9041d59a78b0cb97cbb7a72189eb73ceebc0a730
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-13 11:15:21 +0800

    Update deleteclass.php

--- Student Management system/deleteclass.php
+++ Student Management system/deleteclass.php
@@ -1,12 +1,14 @@
 <?php
 include 'conn.php';
 $class_code = $_GET['class_code'];
+//send sql statemnet to database to delete specific data
 $delete =  "delete from class where class_code='$class_code'";
 if (!mysqli_query($conn, $delete)){
     die ( 'Error: '. mysqli_error($conn));
 }
 else{
     echo "Sucessfully deleted<br>";
+    //send user to classlist
     header('location:classlist.php');
 mysqli_close($conn);
 }
