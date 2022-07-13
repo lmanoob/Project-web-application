14bc01092d155ebaa3a60024d718241168087c7f
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-13 11:14:17 +0800

    Update conn.php

--- Student Management system/conn.php
+++ Student Management system/conn.php
@@ -4,6 +4,7 @@ define('db_password','');
 define('db_host','localhost');
 define('db_name','studentmanagementsystem');
 
+//connect to database and will include this page to every page
 $conn = mysqli_connect (db_host, db_user, db_password, db_name);
 
 if (mysqli_connect_errno())
