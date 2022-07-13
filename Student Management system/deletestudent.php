206f53575f1f77e4b218eda60037fe9a4cfcf789
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-13 11:16:12 +0800

    Update deletestudent.php

--- Student Management system/deletestudent.php
+++ Student Management system/deletestudent.php
@@ -1,13 +1,13 @@
 <?php
 include 'conn.php';
 $id = $_GET['student_id'];
+//send sql statement to delete specific student based on their id
 $delete =  "delete from student where student_id='$id'";
 if (!mysqli_query($conn, $delete)){
     die ( 'Error: '. mysqli_error($conn));
 }
 else{
-    echo "Sucessfully deleted<br>";
-    echo "<a href=studentlist.php>Back to table</a>";
+    //send back to studentlist
     header('location:studentlist.php');
 mysqli_close($conn);
     
