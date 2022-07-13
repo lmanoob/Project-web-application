8663650f6cc466be015ce004cab36a1d06e8fc87
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-13 10:13:27 +0800

    Update add2.php

--- Student Management system/add2.php
+++ Student Management system/add2.php
@@ -1,11 +1,13 @@
 <?php
 include 'conn.php';
 
+//send sql statement to the database
 $sql="insert into teacher (teacher_id, teacher_firstName, teacher_lastName, teacher_email, teacher_password, teacher_phoneNum)
 values ('$_POST[id]','$_POST[firstname]','$_POST[lastname]','$_POST[email]','$_POST[password]','$_POST[phone]')";
 if(!mysqli_query($conn, $sql))
 {
-    die('Error: '.mysqli_error($conn));
+    die('Error: '.mysqli_error($conn));//output the type of error if fail
 }
+// output if sucess
 echo "Successfully registered <br>";
 ?>
