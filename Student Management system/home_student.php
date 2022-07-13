67ce22b9623c581ff03411485fc82d3843090a8a
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-13 11:28:05 +0800

    Update home_student.php

--- Student Management system/home_student.php
+++ Student Management system/home_student.php
@@ -1,13 +1,14 @@
 <?php
 	include 'conn.php';
 	session_start();
-    $id = $_SESSION['student_id'];
+    $id = $_SESSION['student_id'];// set $id to the student_id that is stored in the session
 ?>
 <!Doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <!--link to bootsrap to use the css-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Home</title>
     <style>
@@ -16,13 +17,13 @@
         background-color: aquamarine;
         height:400px;
         background-position: center;
-        background-size: cover;
+        background-size: cover; /*Background image will cover the whole page*/
         background-repeat: no-repeat;
       }
     </style>
   </head>
   <body>
-    <?php include "./header.html" ?>
+    <?php include "./header.html" ?>  <!--header for navbar-->
     <div class="row justify-content-around mx-auto pt-5">
         <div class="col-lg-4">
           <div class="card text-white bg-info mb-3 text-center">
@@ -59,6 +60,7 @@
           </div>
         </div>
       </div>
+	  <!--link to implement javascript bootsrtap-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
