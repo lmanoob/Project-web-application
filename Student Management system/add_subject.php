1ee3c95116e76088fc5b73d73492c979bb4e1de2
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-13 16:04:18 +0800

    Update add_subject.php

--- Student Management system/add_subject.php
+++ Student Management system/add_subject.php
@@ -25,6 +25,36 @@
          padding: 20px;
         }
     </style>
+<script>
+     function validateform()
+{
+	var sub_code= document.register.sub_code.value;
+	var sub_name= document.register.sub_name.value;
+	var sub_desc= document.register.sub_desc.value;
+	var sub_cred= document.register.sub_cred.value;
+	
+	if(sub_code=null||sub_code=="")
+	{
+		alert("Subject Code can't be blank");
+		return false;
+	}
+	else if(sub_name=null||sub_name=="")
+	{
+		alert("Subject Name can't be blank");
+		return false;
+	}
+	else if(sub_desc=null||sub_desc=="")
+	{
+		alert("Subject Description can't be blank");
+		return false;
+	}
+	else if(sub_cred=null||sub_cred=="")
+	{
+		alert("Subject Credit can't be blank");
+		return false;
+	}
+}
+    </script>
   </head>
 <body>
 <?php
@@ -33,7 +63,7 @@ include "./header_teach.html"; //header for navbar
 ?>
 	<div id='kotak-tengah'>
         <table>
-        <form action="add_subjectprocess.php" method = "post">
+       <form name='register' onsubmit='return validateform()' action="add_subjectprocess.php" method = "post">
 			<div class='mb-3'>
             <label for='SuCode' class='form-label'>Subject Code:</label>
 			<input type='text' class='form-control' name='sub_code'>
@@ -51,7 +81,7 @@ include "./header_teach.html"; //header for navbar
 			
 			<div class='mb-3'>
 			<label for='sub_cred' class='form-label'>Subject Credit:</label></th>
-			<input type='text' class='form-control' name='sub_cred'>
+			<input type='number' class='form-control' name='sub_cred'>
 			</div>
 			
            <input type='submit' class='btn btn-primary' value='UPDATE'>
