dcb89826a5aaab419344bf3eea9d3464d0ac6602
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-13 10:14:33 +0800

    Update add_classprocess.php

--- Student Management system/add_classprocess.php
+++ Student Management system/add_classprocess.php
@@ -1,13 +1,16 @@
 <?php
 include 'conn.php';
 
+//send sql statement to the database
 $sql="insert into class (class_code, class_name, number_of_student, subject_code, teacher_id)
 values ('$_POST[classcode]','$_POST[classname]','$_POST[NoS]','$_POST[subject_code]','$_POST[teacher_id]')";
 if(!mysqli_query($conn, $sql))
 {
+    //output the type of error if fail
     die('Error: '.mysqli_error($conn));
 }
 else{
+    // output if sucess
      header('location:classlist.php');
 }
 ?>
