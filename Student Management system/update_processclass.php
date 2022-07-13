1a2c4ae0b01e7145f0914c02cab251718bedb303
itcatmeow <103352895+itcatmeow@users.noreply.github.com>
2022-07-13 12:04:39 +0800

    Update update_processclass.php

--- Student Management system/update_processclass.php
+++ Student Management system/update_processclass.php
@@ -3,6 +3,7 @@ include 'conn.php';
 include './header_teach.html';
 $class_code = $_GET['class_code'];
 $newclasscode=$_POST['code'];
+//send sql statement to the database
 $update = "UPDATE class SET class_code='$_POST[code]',
 class_name='$_POST[classname]',number_of_student='$_POST[NoS]', subject_code ='$_POST[subject_code]',
 teacher_id='$_POST[teacher_id]' where class_code='$class_code'";
@@ -10,6 +11,6 @@ if(!mysqli_query($conn, $update)){ die('Error: '.mysqli_error($conn));
 }
 else{
     header('location:classlist.php');
-	mysqli_close($conn);
+    mysqli_close($conn);
 }
 ?>
