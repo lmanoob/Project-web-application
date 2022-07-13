ce38e8db72b5ee6b69ad0dc185326650ff213031
itcatmeow <103352895+itcatmeow@users.noreply.github.com>
2022-07-13 11:43:22 +0800

    Update viewownsubject.php

--- Student Management system/viewownsubject.php
+++ Student Management system/viewownsubject.php
@@ -20,24 +20,27 @@
 <?php
 	session_start();
 	include 'conn.php';
-    include "./header.html";
-    echo "<br>";
+    	include "./header.html";
+    	echo "<br>";
 	$id= $_SESSION['student_id'];
-    $result = mysqli_query($conn, "SELECT * from subject where subject_code IN (select subject_code from class where class_code IN (select class_code from register where student_id='$id'))");
-    echo "<div class=container>";
-    echo "<div class=row>";
-    echo "<table class=table>";
-    while ($row = mysqli_fetch_row($result)){
+	//send the sql statement to the database
+    	$result = mysqli_query($conn, "SELECT * from subject where subject_code IN (select subject_code from class where class_code IN (select class_code from register where student_id='$id'))");
+    	//display the output
+	echo "<div class=container>";
+    	echo "<div class=row>";
+    	echo "<table class=table>";
+    	while ($row = mysqli_fetch_row($result)){
         echo "<tr class=bg-primary><th>Subject Code</th><th scope=col>$row[0]</th></tr>";
         echo "<tr class=bg-info><th>Subject Name</th><th scope=col>$row[1]</th></tr>";
         echo "<tr class=bg-info><th>Subject Description</th><th scope=col>$row[2]</th></tr>";
         echo "<tr class=bg-info><th>Subject Credit</th><th scope=col>$row[3]</th></tr>";
-    }
-    echo "</table>";
-    echo "</div>";
-    echo "</div>";
-    mysqli_free_result($result);
-    mysqli_close($conn);
+   	}
+    	echo "</table>";
+    	echo "</div>";
+   	echo "</div>";
+	//clear the result & close the connection to the database
+    	mysqli_free_result($result);
+    	mysqli_close($conn);
 
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
