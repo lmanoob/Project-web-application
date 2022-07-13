2531785536e0ec9552e9a92fc9a6a87a6e82985f
lmanoob <103398600+lmanoob@users.noreply.github.com>
2022-07-13 10:17:24 +0800

    add comment

--- Student Management system/register_class.php
+++ Student Management system/register_class.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> //css
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Home</title>
     <style>
@@ -32,13 +32,13 @@
 	include './header.html';
 	
 	$id= $_SESSION['student_id'];
-	$result = mysqli_query($conn, "SELECT class_code FROM class");
-	$result2 = mysqli_query($conn, "SELECT student_id FROM student WHERE student_id='$id'");
+	$result = mysqli_query($conn, "SELECT class_code FROM class"); // class table
+	$result2 = mysqli_query($conn, "SELECT student_id FROM student WHERE student_id='$id'"); //student table
 	$row = mysqli_fetch_row($result2);
 	$row2 = mysqli_fetch_row($result);
 	
 	echo "<div id='kotak-tengah'>";
-    echo "<form action='registerclass.php' method='post'>";
+    echo "<form action='registerclass.php' method='post'>"; //registering for classes form
 	
     echo "<div class='mb-3'>";
     echo "<label for='id' class='form-label'>Student ID:</label>"; 
@@ -46,14 +46,14 @@
     echo "</div>";
 	
 	echo "<div class=mb-3>";
-	echo "<label for='class_code'>Pick A Class:</label><br>";
+	echo "<label for='class_code'>Pick A Class:</label><br>"; // pick the class
 	echo "<select class='form-select' aria-label='Default select example' name='class_code'>";
 	echo "<option selected>Pick A Class</option>";
 	foreach( $result as $row2 ){
 	$class_code=$row2['class_code'];
-	echo "<option value=".$class_code.">" . $row2['class_code'] . "</option>";}
+	echo "<option value=".$class_code.">" . $row2['class_code'] . "</option>";} //classcode based on options
 	echo "</select></div><br>";
-    echo "<input type='submit' class='btn btn-primary' value=Register></form>";
+    echo "<input type='submit' class='btn btn-primary' value=Register></form>"; //submit registration
 	echo "</div>";
 	echo "<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js'></script>";
     echo "</body>";
