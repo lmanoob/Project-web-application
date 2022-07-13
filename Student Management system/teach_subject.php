9c509476f2241ac5fe1e31c6f2503f8f9177e220
lmanoob <103398600+lmanoob@users.noreply.github.com>
2022-07-13 10:26:27 +0800

    add comment

--- Student Management system/teach_subject.php
+++ Student Management system/teach_subject.php
@@ -23,11 +23,11 @@
     include "./header_teach.html";
     echo "<br>";
 	$id= $_SESSION['teacher_id'];
-	$result = mysqli_query($conn, "SELECT * from subject where subject_code IN (select subject_code from class where teacher_id='$id')");
+	$result = mysqli_query($conn, "SELECT * from subject where subject_code IN (select subject_code from class where teacher_id='$id')"); // fetch from database
     echo "<div class=container>";
     echo "<div class=row>";
     echo "<table class=table>";
-	echo "<tr class=bg-secondary><th>Subject Code</th><th>Subject Name</th><th>Subject Description</th><th>Subject Credit</th></tr>";
+	echo "<tr class=bg-secondary><th>Subject Code</th><th>Subject Name</th><th>Subject Description</th><th>Subject Credit</th></tr>"; //showing the subject thought
 while($row = mysqli_fetch_row($result)){
     echo "<tr class=bg-info><th scope=col>$row[0]</th><th scope=col>$row[1]</th><th scope=col>$row[2]</th><th scope=col>$row[3]</th></tr>";
 
@@ -35,7 +35,7 @@ while($row = mysqli_fetch_row($result)){
     echo "</table>";
     echo "</div>";
     echo "</div>";
-	mysqli_free_result($result);
+	mysqli_free_result($result);// show the result from the fetching
 	mysqli_close($conn);
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
