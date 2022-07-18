a2a8d3458190d3ca6467c92aa894b491644086e8
itcatmeow <103352895+itcatmeow@users.noreply.github.com>
2022-07-18 09:52:44 +0800

    Update updatestudent.php

--- Student Management system/updatestudent.php
+++ Student Management system/updatestudent.php
@@ -111,11 +111,11 @@ $row = mysqli_fetch_row($result);
     
     echo "<input type='submit' class='btn btn-primary' value='UPDATE'></form>";
     echo "</div>";
-    echo "<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js'></script>";
-    echo "</body>";
-    echo "</html>";
-
+ 
 //free the result & close the connection
 mysqli_free_result($result);
 mysqli_close($conn);
 ?>
+ <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js'></script>
+ </body>
+ </html>
